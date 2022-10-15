8b576c0f8a2b68f97d8555bce41350159ecb653e
Brunilda Muco <brunildamuco6@gmail.com>
2022-10-15 22:46:12 +0200

    form

---
+++ src/Components/SignInForm.tsx
@@ -0,0 +1,5 @@
+export function SignInForm () {
+    return (
+        <div></div>
+    )
+}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,12 @@
 import { Buttons } from "../Components/Buttons";
+import { SignUpForm } from "../Components/SignUpForm";
 
 export function Home () {
     return (
         <div>
             <Buttons variant="signIn">Sign in</Buttons>
             <Buttons variant="signUp">Sign up</Buttons>
+            <SignUpForm />
         </div>
     )
 }
